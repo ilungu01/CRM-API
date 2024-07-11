be27f67806d9bd43538946a49eb9f0ede64909e6
IGOR L <igorl@directferries.com>
2024-07-11 14:14:48 +0300

    [] temp

---
+++ CRM-API/Controllers/UsersController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRM_API.Controllers;
+
+[ApiController]
+public class UsersController : ControllerBase
+{
+    
+}
---
+++ CRM-API/DTOs/DUser.cs
@@ -0,0 +1,7 @@
+﻿namespace CRM_API.DTOs;
+
+public class DUser
+{
+    public int Id;
+    
+}
