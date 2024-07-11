6189cd42c5b5318e9f08375d8874135bebd1f45d
IGOR L <igorl@directferries.com>
2024-07-11 15:30:07 +0300

    [CRM-3] Add users repo and add first endpoint

---
+++ CRM-API/Services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using CRM_API.DTOs;
+using CRM_API.Entities;
+using CRM_API.Mappers;
+using CRM_API.Repository;
+
+namespace CRM_API.Services;
+
+public class UserService
+{
+    private readonly UserRepository _usersRepo;
+
+    public UserService()
+    {
+        _usersRepo = new UserRepository();
+    }
+
+    public DUser AddUser(DUser newUser)
+    {
+        var userToEntity = Mapper.Instance.Map<DUser, EUser>(newUser);
+        var addUser = _usersRepo.AddUser(userToEntity);
+        return Mapper.Instance.Map<EUser, DUser>(addUser);
+    }
+}
