189ba7c96672db13b601d47f4ccab555a36955ce
IGOR L <igorl@directferries.com>
2024-07-11 16:57:20 +0300

    [CRM-4] Add validator for new user

---
+++ CRM-API/Validations/UserValidator.cs
@@ -0,0 +1,15 @@
+﻿using CRM_API.DTOs;
+using FluentValidation;
+
+namespace CRM_API.Validations;
+
+public class AddUserValidator : AbstractValidator<DUser>
+{
+    public AddUserValidator()
+    {
+        RuleFor(user => user.FirstName).NotNull().NotEmpty();
+        RuleFor(user => user.LastName).NotNull().NotEmpty();
+        RuleFor(user => user.Address).NotNull().NotEmpty();
+        RuleFor(user => user.Age).NotNull().NotEmpty();
+    }
+}
